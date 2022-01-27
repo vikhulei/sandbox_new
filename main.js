9ac53e3c089131cf44751df6cee9bf523df7fccd
vikhulei <65869915+vikhulei@users.noreply.github.com>
2022-01-27 18:12:11 +0200

    files  from css projectuploaded

--- main.js
+++
@@ -1,6 +0,0 @@
-const showSubMenu = () => {
-  document.getElementById("subMenu").style.cssText =
-    "top: 60px; transition: 700ms; opacity: 1";
-};
-
-document.getElementById("about").addEventListener("click", showSubMenu);
